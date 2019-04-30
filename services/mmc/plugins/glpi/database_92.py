4f37de37b592ac2e8632c53dc1086c78932b1d04
Nicolas Lécureuil <neoclust@mageia.org>
2019-04-30 12:06:13 +0200

    Fix ident

--- services/mmc/plugins/glpi/database_92.py
+++ services/mmc/plugins/glpi/database_92.py
@@ -4797,13 +4797,13 @@ ORDER BY
             elif machine['os'].startswith('Ubuntu'):
                 machine['os'] = 'Ubuntu'
                 # We want just the XX.yy version number
-            try:
-                machine['version'] = machine['version'].split(" ")[0].split(".")
-            except AttributeError:
-                machine['version'] = ""
+                try:
+                    machine['version'] = machine['version'].split(" ")[0].split(".")
+                except AttributeError:
+                    machine['version'] = ""
                 if len(machine['version']) >= 2:
                     machine['version'] = machine['version'][0:2]
-                machine['version'] = '.'.join(machine['version'])
+                    machine['version'] = '.'.join(machine['version'])
             elif machine['os'].startswith('Mageia'):
                 machine['os'] = machine['os'].split(" ")[0]
             elif machine['os'].startswith('Unknown'):
